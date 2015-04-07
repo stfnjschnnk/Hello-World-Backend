0afe74282c07956699171224a3e251f16a2b3125
stfnjschnnk <stefan.jeschonnek@gmail.com>
2015-04-07 13:08:16 +0200

    Added Keypress event handler

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var main = function() {
+    $('.status-box').focus();
+    $('.btn').click(askServerForUser);
+    $(document).keypress(function(event) {
+        if (event.which === 13) {
+            askServerForUser();
+            event.preventDefault();
+            // TODO make event not execute 
 
-    $('.btn').click(function() {
-        /*alert('button was clicked')*/
-        var firstName = $('.status-box').val();
-
-
-        $.getJSON("http://localhost:8080/HelloWorldBackend?firstName="+firstName, function(result){
-            $('.results').html(result.firstName+" "+result.lastName+"<br><br>"+result.statement);
-        });
+        }
     });
 }
 
 $(document).ready(main);
+
+function askServerForUser() {
+    var firstName = $('.status-box').val();
+    $.getJSON("http://localhost:8080/HelloWorldBackend?firstName="+firstName, function(result) {
+        $('.results').html(result.firstName+" "+result.lastName+"<br><br>"+result.statement);
+    });
+    $('.status-box').val('');
+}
